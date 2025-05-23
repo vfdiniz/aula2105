89fa81219aee30047e35eacde0133e9a24ed6806
vfdiniz <sazykkj@gmail.com>
2025-05-23 09:31:05 -0300

    Update desafio3.java

--- desafio3.java
+++ desafio3.java
@@ -5,26 +5,26 @@ import java.util.Scanner;
 public class desafio3 {
 	public static void main(String[] args) {
 		Scanner ler = new Scanner (System.in);
-		int a [] = new int[2];
-		int b [] = new int [2];
-		int c [] = new int [4];
+		int valor1 [] = new int[2];
+		int valor2 [] = new int [2];
+		int valor3 [] = new int [4];
 		
 	for (int i=0; i<2; i++) {
 		System.out.println("Informe o" +i + "° valor..: ");
-		a [i] = ler.nextInt();
-		c [i] = a [i]; 
+		valor1 [i] = ler.nextInt();
+		valor3 [i] = valor1 [i]; 
 		
 	}
 	for (int i =0; i<2; i++) {
 		System.out.println("Informe o " + i + "° valor..:");
-		b [i] = ler.nextInt();
-		c [i+2] = b [i];
+		valor2 [i] = ler.nextInt();
+		valor3 [i+2] = valor2 [i];
 		
 	}
 
 	
 			for (int i =0; i<4; i++) {
-				System.out.println("Informe o " + i + "° valor..:" + c[i]);
+				System.out.println("Informe o " + i + "° valor..:" + valor3[i]);
 				
 		
 	}
